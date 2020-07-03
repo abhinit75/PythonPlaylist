dbb5bc51303ed566a43058c571a313615216da0b
Abhinit Patil <abhineet0309@gmail.com>
2020-07-02 22:02:11 -0400

    New Ipnyb test

--- PythonAlgorithms/Fibonacci.py
+++ PythonAlgorithms/Fibonacci.py
@@ -1,5 +1,5 @@
 # Studying the Fibonacci in depth
-
+'''
 #Attempt 1
 
 nums = int(input())
@@ -20,12 +20,75 @@ def fibonnaci(numVals):
 fibonnaci(nums)
 
 # Recursion Method
-n = 0
-d = 1
+# returns the nth number
 
 def fibrecurse(n):
-    if n == 0:
-        return n
+    if n == 1 or n == 2:
+        return 1
     else:
-        print(n+d)
-        return fibrecurse(n = d)
+        return fibrecurse(n-2) + fibrecurse(n-1)
+
+print(fibrecurse(nums))
+
+'''
+'''
+Very inefficient
+- fibonnaci(5) --> fibonnaci (4) --> onwards
+- fibonacci (3) + fibonnaci(2) + fibonnaci(2) + fibonnaci(1)
+- fibonnaci(2) + fibonnaci(1) + fobonacci(2) fibonnaci(2) + fibonnaci(1)
+- finally - 1 + 1 + 1 + 1 + 1
+
+'''
+
+
+# Memoization
+fibonacci_cache = {}
+
+def fibonacci(n):
+    #If we have cached the value, then return it
+    if n in fibonacci_cache:
+        return fibonacci_cache[n]
+
+    # Compute the Nth term
+    if n == 1:
+        value = 1
+    elif n == 2:
+        value = 1
+    elif n > 2:
+        value = fibonacci(n - 1) + fibonacci(n - 2)
+
+    # Cache the value and return it
+    fibonacci_cache[n] = value
+    return value
+
+for n in range(1, 101):
+    print(n, ":", fibonacci(n))
+
+
+# Factorial program with memoization using
+# decorators.
+
+# A decorator function for function 'f' passed
+# as parameter
+def memoize_factorial(f):
+    memory = {}
+
+    # This inner function has access to memory
+    # and 'f'
+    def inner(num):
+        if num not in memory:
+            memory[num] = f(num)
+        return memory[num]
+
+    return inner
+
+
+@memoize_factorial
+def facto(num):
+    if num == 1:
+        return 1
+    else:
+        return num * facto(num - 1)
+
+
+print(facto(5))
